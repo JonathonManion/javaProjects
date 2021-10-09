4f055eb441e3d6bf06f2c28455308d3041e8d2a2
HGP38 <hgp38@Thes-MacBook-Air-3.local>
2021-10-08 18:11:22 -0700

    first commit

---
+++ myProjects/src/myProjects/geniusjokes.java
@@ -0,0 +1,10 @@
+package myProjects;
+
+public class geniusjokes {
+	public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Welcome, my name is Jonathon");//Print My Welcome Message and Name
+        System.out.println("What's the difference between dogs and donuts?"); //Print My Joke's setup
+        System.out.println("If you had to think about it for more than a second you might have a few problems"); //Joke Punch Line
+    }
+}
